84dd5923bff8ee3fe6186e1a2a1920833073edb7
7hills <yedukondalusolleti2709@gmail.com>
2024-01-09 13:45:11 +0530

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+// import Logo from "../../images/logo.svg";
+import MenuIcon from "@mui/icons-material/Menu";
+import { NavLink } from "react-router-dom";
+import "../../styles/HeaderStyles.css";
+const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  // hndle menu click
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  //menu drawer
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography
+        color={"goldenrod"}
+        variant="h6"
+        component="div"
+        sx={{ flexGrow: 1, my: 2 }}
+      >
+       <FastfoodIcon />
+        {/* <img src={Logo} alt="logo" height={"70"} width="200" /> */}
+        Gismat Arabic Restaurent
+      </Typography>
+      <Divider />
+
+    
+      <ul className="mobile-navigation">
+        <li>
+          <NavLink activeClassName="active" to={"/"}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/menu"}>Menu</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/about"}>About</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/contact"}>Contact</NavLink>
+        </li>
+      </ul>
+     
+    </Box>
+  );
+  return (
+    <>
+      <Box>
+        <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{
+                mr: 2,
+                display: { sm: "none" },
+              }}
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color={"goldenrod"}
+              variant="h4"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+                 <FastfoodIcon />
+              {/* <img src={Logo} alt="logo" height={"70"} width="250" /> */}
+              Gismat Arabic Restaurent
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <ul className="navigation-menu">
+                <li>
+                  <NavLink activeClassName="active" to={"/"}>
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/menu"}>Menu</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/about"}>About</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/contact"}>Contact</NavLink>
+                </li>
+              </ul>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: "240px",
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box>
+          <Toolbar />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/pages/Menu.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { MenuList } from "../data/data";
+import Layout from "./../components/Layout/Layout";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import Button from '@mui/material/Button';
+const Menu = () => {
+  return (
+    <Layout>
+      <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+        {MenuList.map((menu) => (
+          <Card sx={{ maxWidth: "390px", display: "flex", m: 2 }}>
+            <CardActionArea>
+              <CardMedia
+                sx={{ minHeight: "400px" }}
+                component={"img"}
+                src={menu.image}
+                alt={menu.name}
+              />
+              <CardContent>
+                <Typography variant="h5" gutterBottom component={"div"}>
+                  {menu.name}
+                </Typography>
+                <Typography variant="body2">{menu.description}</Typography>
+                <Typography variant="h4">{menu.price}&#8377;</Typography>
+              </CardContent>
+              <Button variant="contained">Add To Cart</Button>
+            </CardActionArea>
+          </Card>
+            
+        ))}
+         
+      </Box>
+    </Layout>
+  );
+};
+
+export default Menu;
